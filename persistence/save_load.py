4bba806be0d4c6634b8c2b5cde8a7498a6d019fb
michaeljschnabel11 <mjschnab@gmail.com>
2026-03-09 20:18:14 -0700

    created save_load.py. No significant code, just used for JSON I/O

---
+++ persistence/save_load.py
@@ -0,0 +1,15 @@
+############################################################################################
+# save_load.py
+#
+# Last edit: Michael Schnabel
+# Last updated: 
+#
+# Thin persistence interface. Delegates to engine/score.py for JSON I/O.
+############################################################################################
+
+from engine.score import load_scores, save_scores
+
+
+'''To use:
+
+from persistence.save_load import load_scores, save_scores'''
